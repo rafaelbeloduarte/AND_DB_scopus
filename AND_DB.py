3edb696a58616d68e52326cf98d0688c97faeb1d
Rafael Belo Duarte <rafaelbeloduarte@protonmail.com>
2022-09-14 10:17:41 -0300

    removing some useless comments

--- AND_DB.py
+++ AND_DB.py
@@ -2,28 +2,19 @@ import pyreadr
 from collections import Counter
 from pybliometrics.scopus import AuthorRetrieval
 
-result = pyreadr.read_r('Bibliometrix-Export-File-2022-09-06.RData') # also works for Rds
-
-# done! let's see what we got
 # result is a dictionary where keys are the name of objects and the values python
 # objects
-#print(result.keys()) # let's check what objects we got
+result = pyreadr.read_r('Bibliometrix-Export-File-2022-09-06.RData') # also works for Rds
 M = result["M"] # extract the pandas data frame for object M
 
-# print(dir(M))
-# print(M.RP)
-
-# criando uma lista com todos os autores
+# initialize lists of all authors names and IDs
 all_authors_repeated = []
 all_AU_ID_repeated = []
 
-# objeto author ID
+# get author ID objetc
 AU_ID = getattr(M, 'Author.s..ID')
-#corr_addr = getattr(M, 'Correspondence Address')
-# print(M.AU)
-authors_addr = {}
-authors_addr_list = []
 
+# create lists of IDs and authors in the order of appearance
 for i in range(len(M)):
 	authors_tmp = []
 	AU_ID_tmp = []
@@ -33,23 +24,13 @@ for i in range(len(M)):
 	
 	authors_tmp = authors_tmp.split(";")
 	AU_ID_tmp = AU_ID_tmp.split(";")
-	#print(AU_ID_tmp)
-	#print(authors_tmp)
+	
 	for j in range(len(authors_tmp)):
 		all_authors_repeated.append(authors_tmp[j])
 	for j in range(len(AU_ID_tmp)):
 		all_AU_ID_repeated.append(AU_ID_tmp[j])
-		
-	# list of authors correspondance addresses
-	
-	# authors_addr_tmp = M.RP[i]
-	
-	# print(authors_addr_tmp)
-	# authors_addr_split = authors_addr_tmp.split('\n')
-	
-	# for i in range(len(authors_addr_split)):
-	# 	authors_addr_list.append(authors_addr_split[i].split(';'))
 
+# filtering empty items
 temp_list = all_AU_ID_repeated
 all_AU_ID_repeated = list(filter(None, temp_list))
 
@@ -59,41 +40,26 @@ all_authors_repeated = list(filter(None, temp_list))
 print("Authors string count: ", len(all_authors_repeated))
 print("Authors IDs count: ", len(all_AU_ID_repeated))
 
-# print(authors_addr_list[50][0])
-# authors_addr_list_names = []
-# authors_addr_list_addr = []
-# for i in range(len(authors_addr_list)):
-	# try:
-		# authors_addr_list_names.append(authors_addr_list[i][0])
-		# authors_addr_list_addr.append(authors_addr_list[i][1])
-	# except Exception as e: 
-		# print(e)
-
-# check for duplicates in authors_addr_list
-# import collections
-# print("Repeated correspondance names:")
-# print([item for item, count in collections.Counter(authors_addr_list_names).items() if count > 1])
-
-# Python 3 code to demonstrate
 # removing duplicate elements from the list
 all_authors = [*set(all_authors_repeated)]
 todos_AU_ID = [*set(all_AU_ID_repeated)]
-# todos_AU_ID.remove('')
-#print("List after removing duplicate elements: ", all_authors)
+
 print("Total unique author strings: ", len(all_authors))
 print("Total unique IDs: ", len(todos_AU_ID))
 
 # creating a list to count all the times a name repeats
 author_name_repeat_count = [0] * len(all_authors)
+
 for i in range(len(all_authors)):
 	for j in range(len(all_authors_repeated)):
 		if all_authors[i] in all_authors_repeated[j]:
 			# print(all_authors[i], "repeats at position ", j)
 			author_name_repeat_count[i] = author_name_repeat_count[i] + 1
 
-# creating a list of authors that appear more than once in data set
+# creating a list of authors that appear more than once in the data set
 repeated_authors = []
 unique_authors = []
+
 for i in range(len(author_name_repeat_count)):
 	if author_name_repeat_count[i] > 1:
 		repeated_authors.append(all_authors[i])
@@ -105,6 +71,7 @@ print("Number of unique authors: ", len(unique_authors))
 
 # generate a list of IDs coupled to each unique name
 authors_coupled_ID_list = []
+
 for k in all_authors:
 	name_IDs = []
 	name_IDs.append(k)
@@ -117,14 +84,13 @@ for k in all_authors:
 
 # count number of unique IDs for each name
 authors_more_than_1_ID = []
-# print("Below are all authors unique names with more than 1 ID:")
+print("Below are all authors unique names with more than 1 ID:")
 f = open("authors_with_multiple_IDs.csv", "w")
 for i in authors_coupled_ID_list:
 	elements = list(Counter(i).keys()) # equals to list(set(words))
 	frequency = list(Counter(i).values()) # counts the elements' frequency
 	if len(elements) > 2:
 		# print("Author ", elements[0], " has ", len(elements) - 1, "IDs: ", elements[1:len(elements)])
-		#authors_more_than_1_ID.append(elements)
 		#print(elements)
 		#print("Author's IDs: ", list(elements))
 		#print("Element count: ", list(frequency))
@@ -134,6 +100,4 @@ for i in authors_coupled_ID_list:
 			some_author = AuthorRetrieval(elements[j])
 			information = ";" + elements[j] + ";" + some_author.surname + ", " + some_author.given_name + "; Publication range: " + str(some_author.publication_range) + " Orcid: " + str(some_author.orcid) + " Date created: " + str(some_author.date_created)
 			f.write(information)
-			#print(elements[j])
-			#print("Surname Given Name: ", some_author.surname, some_author.given_name, "Name variants: ", some_author.name_variants)
 f.close()
